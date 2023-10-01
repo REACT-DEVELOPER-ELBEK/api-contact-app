8dae89e26e4dfacf410f98c5c11c7917c4dfd5af
Yusupov Elbek <elbeky829@gmail.com>
2023-10-02 00:24:20 +0500

    adding to api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,85 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Style.scss";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import { RiContactsFill } from "react-icons/ri";
 import { BsFillTelephoneFill } from "react-icons/bs";
+import axios from "axios";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [firstNameArr, setFirstNameArr] = useState([]);
-
-  const [lastName, setLastName] = useState("");
-  const [lastNameArr, setLastNameArr] = useState([]);
-
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [phoneNumberArr, setPhoneNumberArr] = useState([]);
-
-  function getValue(e) {
-    setFirstNameArr([...firstNameArr, firstName]);
-    setLastNameArr([...lastNameArr, lastName]);
-    setPhoneNumberArr([...phoneNumberArr, phoneNumber]);
-    e.preventDefault();
+  // useEffect(() => {
+  //   axios("http://localhost:3000/users")
+  //     .then((response) => setPost(response.data))
+  //     .catch((err) => console.log(err));
+  // }, []);
+  const [post, setPost] = useState({
+    firstName: "",
+    lasName: "",
+    phoneNumber: ""
+  });
+  function getValue(event) {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/users", { post })
+      .then((response) => setPost(response.data))
+      .catch((err) => console.log(err));
   }
-
+  function inputValue(event){
+    setPost({[event.target.name]: event.target.value });
+  };
   return (
     <div className="container">
       <form>
         <div className="input">
-        <label><RiContactsFill /></label>
-        <Form.Control
-          type="text"
-          style={{ width: "250px" }}
-          placeholder="First name"
-          onChange={(e) => setFirstName(e.target.value)}
-        />
+          <label>
+            <RiContactsFill />
+          </label>
+          <Form.Control
+            type="text"
+            style={{ width: "250px" }}
+            placeholder="First name"
+            onChange={inputValue}
+            name="firstName"
+          />
         </div>
         <div className="input">
-        <label><RiContactsFill /></label>
-        <Form.Control
-          type="text"
-          style={{ width: "250px" }}
-          placeholder="Last name"
-          onChange={(e) => setLastName(e.target.value)}
-        />
+          <label>
+            <RiContactsFill />
+          </label>
+          <Form.Control
+            type="text"
+            style={{ width: "250px" }}
+            placeholder="Last name"
+            onChange={inputValue}
+            name="lastName"
+          />
         </div>
         <div className="input">
-        <label><BsFillTelephoneFill /></label>
-        <Form.Control
-          type="tel"
-          style={{ width: "250px" }}
-          placeholder="Phone number"
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
+          <label>
+            <BsFillTelephoneFill />
+          </label>
+          <Form.Control
+            type="tel"
+            style={{ width: "250px" }}
+            placeholder="Phone number"
+            onChange={inputValue}
+            name="phoneNumber"
+          />
         </div>
         <Button variant="outline-primary" onClick={getValue} type="submit">
           Add contact
         </Button>
       </form>
+      <div className="contact__list">
+        <div className="contact__item">
+          {
+            post.map((item, index)=>{
+              return (<p key={index}>{item.post.firstName }</p>)
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
